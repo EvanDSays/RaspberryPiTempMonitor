6e9fc530465b1f82bd7340bcd97197b94cffeb5b
EvanDSays <evandavis@gmail.com>
2020-06-06 21:10:47 -0500

    Update Send.Temp.To.Adafruit.IO.py

--- Send.Temp.To.Adafruit.IO.py
+++ Send.Temp.To.Adafruit.IO.py
@@ -3,7 +3,7 @@ import glob
 import time
 from Adafruit_IO import Client, Feed, Data, RequestError
 
-#Read temperature from DS18B20 sensor
+#Read temperature from sensor
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
@@ -21,10 +21,18 @@ if equals_pos != -1:
     temp_c = float(temp_string) / 1000.0
     temp_f = temp_c * 9.0 / 5.0 + 32.0
 
-#Send data to Adafruit IO
+#Send data to Adafruit.IO
+
+#CHANGE THESE THREE VARIABLES
 ADAFRUIT_IO_USERNAME = "YOUR_ADAFRUIT_IO_USERNAME_HERE"
 ADAFRUIT_IO_KEY = "YOUR_ADAFRUIT_IO_KEY_HERE"
 ADAFRUIT_FEED = "YOUR_ADAFRUIT_FEED_HERE"
+
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+
+#If you want to use Celcius, uncomment the below line and comment out the line with temp_f
+#data = Data(value=temp_c)
 data = Data(value=temp_f)
+
+#Sends the data to Adafruit.IO
 aio.create_data(ADAFRUIT_FEED, data)
